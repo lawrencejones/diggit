9f7c4b0283891b4734dab495563e91ffa3fe36b0
Lawrence Jones <lawrjone@gmail.com>
2015-12-28 22:35:43 +0000

    Snapshot retrieval

--- code-overview/main.js
+++ code-overview/main.js
@@ -1,8 +1,25 @@
 'use strict';
 
+if (process.argv.length !== 3) {
+  console.error('Requires path to data.json');
+  process.exit(255);
+}
+
+const fs = require('fs');
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+
+const loadSnapshot = (dataFile) => {
+  if (!fs.existsSync(dataFile)) {
+    console.error(`Could not find file ${dataFile}`);
+    process.exit(255);
+  }
+
+  return require(dataFile);
+};
+
+global.snapshot = loadSnapshot(process.argv[2]);
 
 let mainWindow;
 
@@ -12,4 +29,5 @@ app.on('ready', () => {
   mainWindow = new BrowserWindow({width: 800, height: 600});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
   mainWindow.on('closed', () => mainWindow = null);
+  mainWindow.hello = 'world';
 });
--- code-overview/vis.js
+++ code-overview/vis.js
@@ -1,1 +1,6 @@
-alert('lol');
+'use strict';
+
+const remote = require('remote');
+const snapshot = remote.getGlobal('snapshot');
+
+$('body').append(`<pre>${JSON.stringify(snapshot, null, 2)}</pre>`);
