5ea270a440d9a4ce1cb273c7c9a415e79d19712b
Lawrence Jones <lawrjone@gmail.com>
2016-01-12 22:50:10 +0000

    Neaten + specs for rendering

--- code-overview/client/renderer.js
+++ code-overview/client/renderer.js
@@ -1,6 +1,50 @@
 'use strict';
+/* Rendering tools for the code-overview display */
 
 const _ = require('lodash');
+
+/* Given a root frame of {score, entries}, will generate rectangles for each subframe
+ * that fill the space defined as [x, y, w, h]. */
+const generateFrameRectangles = (rootFrame, [x, y, w, h], drawDirectories) => {
+  if (_.isUndefined(drawDirectories)) { let drawDirectories = false; }
+
+  if (rootFrame.entries.length === 1) {
+    let frame = rootFrame.entries[0];
+    let rects = [];
+
+    /* Only output this rectangle if we're drawing directories, or it's not a directory */
+    if (drawDirectories || !_.isObject(frame.entries)) {
+      rects.push({
+        label: frame.path,
+        score: frame.score,
+        x, y, w, h,
+      });
+    }
+
+    if (_.isObject(frame.entries)) {
+      rects = rects.concat(generateFrameRectangles({
+        score: frame.score,
+        entries: _.values(frame.entries),
+      }, [x, y, w, h], drawDirectories));
+    }
+
+    return rects;
+  }
+
+  return _
+    .chain(evenlySplit(rootFrame.entries, 'score', 2))
+    .reduce(function(rects, {score, items}) {
+      let dims = splitDimensions(this.totalDim, score / this.totalScore);
+
+      this.totalScore -= score;  // remove this item from the available space
+      this.totalDim = dims[1];  // set remaining space to dimensions from after split
+
+      return [...rects, generateFrameRectangles({ score, entries: items }, dims[0], drawDirectories)];
+
+    }, [], {totalDim: [x, y, w, h], totalScore: rootFrame.score})
+    .flatten()
+    .value()
+};
 
 /* Given a list of items, divides into n partitions that are approximately equal sizing */
 const evenlySplit = (items, scoreKey, n) => {
@@ -35,28 +79,5 @@ const splitDimensions = ([x, y, w, h], ratio) => {
 };
 
 
-const balance = (rootFrame, [x, y, w, h]) => {
-  if (rootFrame.entries.length === 1) {
-    return [{
-      label: rootFrame.entries[0].path,
-      score: rootFrame.score,
-      x, y, w, h,
-    }];
-  }
 
-  return _
-    .chain(evenlySplit(rootFrame.entries, 'score', 2))
-    .reduce(function(rects, {score, items}) {
-      let dims = splitDimensions(this.totalDim, score / this.totalScore);
-
-      this.totalScore -= score;  // remove this item from the available space
-      this.totalDim = dims[1];  // set remaining space to dimensions from after split
-
-      return [...rects, balance({ score, entries: items }, dims[0])];
-
-    }, [], {totalDim: [x, y, w, h], totalScore: rootFrame.score})
-    .flatten()
-    .value()
-};
-
-module.exports = { balance };
+module.exports = { generateFrameRectangles, evenlySplit, splitDimensions };
