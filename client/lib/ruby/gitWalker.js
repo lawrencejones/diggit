ce97276103ce3a7e8570c515a9e81dd65bf70cac
Lawrence Jones <lawrjone@gmail.com>
2016-02-15 09:42:55 +0000

    Extend gitWalker.js to handle file globs

--- client/lib/ruby/gitWalker.js
+++ client/lib/ruby/gitWalker.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const P = require('bluebird');
-const {exec} = require('child_process');
+const {spawn} = require('child_process');
 
 const GIT_WALKER = path.join(__dirname, '../../..', 'utils', 'git_walker.rb');
 
@@ -11,11 +11,31 @@ if (!fs.existsSync(GIT_WALKER)) {
   throw new Error(`Cannot locate git_walker.rb at ${GIT_WALKER}`);
 }
 
-const gitWalker = (repoPath, metric) => {
-  return P.promisify(exec)(`${GIT_WALKER} walk ${repoPath} --metric ${metric}`, {
-    maxBuffer: 20 * 1024 * 1024,  // 20MB
-  }).then((stdout) => {
-    return JSON.parse(stdout);
+const collectStdio = (prog) => {
+  let io = { stdout: '', stderr: '' };
+
+  prog.stdout.setEncoding('utf8')
+  prog.stderr.setEncoding('utf8')
+
+  prog.stdout.on('data', (data) => { io.stdout += data });
+  prog.stderr.on('data', (data) => { io.stderr += data });
+
+  return io;
+}
+
+const gitWalker = (repoPath, metric, pattern) => {
+  return new P((resolve, reject) => {
+    let prog = spawn(GIT_WALKER, [
+      'walk', repoPath,
+      '--metric', metric,
+      '--pattern', pattern || '**/*',
+    ]);
+
+    let io = collectStdio(prog);
+
+    prog.on('close', (code) => {
+      code === 0 ?  resolve(JSON.parse(io.stdout)) : reject(io.stderr);
+    });
   });
 };
 
