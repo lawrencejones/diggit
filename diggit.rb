eef402845b25e6c657ba31a93e90ab5ef68f1286
Lawrence Jones <lawrjone@gmail.com>
2016-03-29 23:15:11 +0100

    Que and ActiveRecord

--- diggit.rb
+++ diggit.rb
@@ -1,13 +1,19 @@
 require 'sinatra'
+require 'sinatra/activerecord'
+require 'que'
 require 'json'
 require 'prius'
 
 # Environment variables
+Prius.load(:diggit_env, env_var: 'RUBY_ENV')
 Prius.load(:diggit_domain)
 Prius.load(:diggit_github_token)
 
 module Diggit
   class App < Sinatra::Base
+    register Sinatra::ActiveRecordExtension
+    Que.connection = ActiveRecord
+    Que.mode = Prius.get(:diggit_env) == 'production' ? :async : :sync
     enable :logging
 
     before '/webhooks/github' do
