af728060f3a0b68f274cfdcb0b3bbaa69441f65f
Lawrence Jones <lawrjone@gmail.com>
2016-04-03 23:51:02 +0100

    Remove dummy js content while we're here

--- web/client/lib/predicateParser.js
+++
@@ -1,9 +0,0 @@
-import PEG from 'pegjs';
-import grammar from './grammar.pegjs!text'
-
-/* Must be able to parse expressions like...
- *
- * \forall x (Foo(x) -> \exists y Bar(x, y))
- *
- */
-export const Parser = PEG.buildParser(grammar);
