0ded2403698bd8081b9e26bad54503d8974435f1
Lawrence Jones <lawrjone@gmail.com>
2016-04-05 20:03:04 +0100

    Working buttons, but broken browserSync
    Need to find where browserSync has broken, and tidy up this whole
    commit. The general approach clearly works however.

--- web/client/routes/projects/index/projects-index.controller.js
+++ web/client/routes/projects/index/projects-index.controller.js
@@ -1,10 +1,28 @@
 import angular from 'angular';
+import 'angular-ui-router';
+import _ from 'lodash';
+
+import {projectsModule} from '../../../services/projects.js'
 
 export const projectsIndexControllerModule = angular.module('projectsIndexControllerModule', [
+  'ui.router',
+  projectsModule.name,
 ])
-.controller('ProjectsIndexController', function($log, projects) {
+.controller('ProjectsIndexController', function($log, $state, $window, projects, Projects) {
   let ctrl = this;
 
-  $log.info(`Received ${projects.projects.length} projects`);
-  ctrl.projects = projects.projects;
+  $log.debug(`Received ${projects.projects.length} projects`);
+
+  _.extend(ctrl, {
+    projects: projects.projects,
+    updateProject: (project, fields) => {
+      let [owner, repo] = project.gh_path.split('/');
+      Projects.update({params: {owner, repo}, data: {projects: fields}})
+        .then(() => {
+          $log.debug(`Successfully updated project!`);
+          $state.reload();
+        })
+        .catch((err) => { $window.alert(`Failed to update project with ${err}`); });
+    },
+  });
 });
