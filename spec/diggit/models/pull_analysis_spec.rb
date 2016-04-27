85f36e17490a893c7f1bb554848f9b681140a2bc
Lawrence Jones <lawrjone@gmail.com>
2016-04-27 14:47:04 +0100

    Fixes bad git reference, empty comment errors (#56)
    Empty comment errors
    (https://rollbar.com/lawrencejones/diggit-prod/items/12/) were from
    ActiveRecord validating presence of pull_analysis.comments prior to
    save, despite a database default being in place.
    
    Bad git reference errors
    (https://rollbar.com/lawrencejones/diggit-prod/items/10/) caused by
    rebasing before the job had been processed, meaning the references
    in the pr were no longer found in the repo.

--- spec/diggit/models/pull_analysis_spec.rb
+++ spec/diggit/models/pull_analysis_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe(PullAnalysis) do
 
     it { is_expected.to be_valid }
     it { is_expected.to validate_presence_of(:pull) }
-    it { is_expected.to validate_presence_of(:comments) }
     it { is_expected.to validate_uniqueness_of(:pull).scoped_to(:project_id) }
 
     it { is_expected.to belong_to(:project) }
