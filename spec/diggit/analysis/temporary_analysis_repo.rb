033119e8326a0eecaedeb0fcbd488db6edb4c533
Lawrence Jones <lawrjone@gmail.com>
2016-05-10 10:55:10 +0100

    TemporaryAnalysisRepo uses Rugged

--- spec/diggit/analysis/temporary_analysis_repo.rb
+++ spec/diggit/analysis/temporary_analysis_repo.rb
@@ -1,49 +1,55 @@
-require 'git'
+require 'rugged'
 require 'fileutils'
-require 'diggit/services/environment'
 
 class TemporaryAnalysisRepo
   @repos = []
 
-  def self.create(&block)
-    new.tap { |repo| block.call(repo) }.g
-  end
-
-  def self.clean!
-    @repos.map { |repo| FileUtils.rm_rf(repo.base) }
-  end
-
-  def self.schedule_for_removal(repo)
-    @repos << repo
-  end
+  def self.create(&block); new.tap(&block).repo; end
+  def self.clean!(); @repos.map(&:destroy); end
+  def self.schedule_for_removal(repo); @repos << repo; end
 
   def initialize(base = Dir.mktmpdir)
-    @base = base
-    @g = Git.init(base)
+    @repo = Rugged::Repository.init_at(base)
+    commit('Initial commit')
     self.class.schedule_for_removal(self)
   end
 
-  attr_reader :base, :g
+  attr_reader :repo
 
-  def write(file, contents)
-    File.write(File.join(base, file), contents)
+  def branch(branch)
+    repo.branches.create(branch, 'HEAD')
+    repo.checkout(branch)
   end
 
-  def rm(file)
-    File.unlink(File.join(base, file))
-  end
-
-  def commit(message, time: Time.now)
-    Diggit::Services::Environment.with_temporary_env(git_time_env(time)) do
-      g.add(all: true)
-      g.commit_all(message)
+  def write(path, contents)
+    repo.index.tap do |index|
+      File.write(File.join(repo.workdir, path), contents)
+      index.add(path: path, oid: Rugged::Blob.from_workdir(repo, path), mode: 0100644)
     end
   end
 
-  private
+  def rm(path)
+    repo.index.tap do |index|
+      File.unlink(File.join(repo.workdir, path))
+      index.remove(path)
+    end
+  end
 
-  def git_time_env(time)
-    { 'GIT_AUTHOR_DATE' => time.to_s,
-      'GIT_COMMITTER_DATE' => time.to_s }
+  def commit(message, time: Time.now)
+    commit_tree = repo.index.write_tree(repo)
+    repo.index.write
+
+    person = { email: 'git@test.com', name: 'Test', time: time }
+    Rugged::Commit.
+      create(repo,
+             message: message,
+             tree: commit_tree,
+             author: person, committer: person,
+             parents: repo.empty? ? [] : [repo.head.target].compact,
+             update_ref: 'HEAD')
+  end
+
+  def destroy
+    FileUtils.rm_rf(repo.workdir)
   end
 end
