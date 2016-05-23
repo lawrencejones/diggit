64f3ad52eb7f09e7a72631e23044530f496a85c6
Lawrence Jones <lawrjone@gmail.com>
2016-05-23 12:05:00 +0100

    Circle ssh patcher

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :branch, 'master'
 set :stage, 'production'
 
 server '178.62.124.191', user: 'deploy', roles: %w(web app)
-server '146.169.47.204', user: 'worker', roler: %w(worker)
+server '146.169.47.204', user: 'worker', roles: %w(worker)
